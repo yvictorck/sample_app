6cfc85369998ac2b00a46db14d477692be891ba9
Victor Chen <yvictorck@gmail.com>
2013-07-14 18:18:44 -0400

    add user micrposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user_ch9_ex, only: [:new, :create]
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   def index
      # @users = User.all
@@ -51,13 +52,7 @@ class UsersController < ApplicationController
   end
   private
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-      # redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
+    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
--- spec/factories.rb
+++ spec/factories.rb
@@ -19,4 +19,9 @@ FactoryGirl.define do
 
     
   end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
 end
